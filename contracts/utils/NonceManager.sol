3452d96222be626b09d27ca77a46716963b72a7f
nnlgsakib <nlgarts@gmail.com>
2025-02-22 18:10:32 +0600

    updated nonce manager

--- contracts/utils/NonceManager.sol
+++ contracts/utils/NonceManager.sol
@@ -2,16 +2,29 @@
 pragma solidity ^0.8.0;
 
 library NonceManager {
-    struct Nonces {
-        uint256 value;
+    // Define a storage mapping to track nonces for each address
+    struct NonceStorage {
+        mapping(address => uint256) nonces;
     }
 
+    // Storage slot identifier for the nonce mapping
+    bytes32 private constant NONCE_STORAGE_POSITION = keccak256("NonceManager.storage");
+
+    // Helper function to access the storage slot
+    function _nonceStorage() internal pure returns (NonceStorage storage ns) {
+        bytes32 position = NONCE_STORAGE_POSITION;
+        assembly {
+            ns.slot := position
+        }
+    }
+
+    // Get the nonce for a given account
     function getNonce(address account) internal view returns (uint256) {
-        return Nonces(account).value;
+        return _nonceStorage().nonces[account];
     }
 
+    // Increment the nonce for a given account
     function incrementNonce(address account) internal {
-        Nonces storage nonces = Nonces(account);
-        nonces.value++;
+        _nonceStorage().nonces[account]++;
     }
 }
